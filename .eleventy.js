fdbc08debf89e4a60ca5c4785573398740e76925
Nina Kin <matikin9@gmail.com>
2024-04-04 21:56:24 -0700

    update default redirect

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
-
 	eleventyConfig.addPassthroughCopy("src/images");
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/js");
+	eleventyConfig.addPassthroughCopy("favicon.ico");
 
 	eleventyConfig.addDataExtension("csv", (contents) => {
 		const records = parse(contents, {
@@ -14,10 +15,12 @@ module.exports = function(eleventyConfig) {
 		return records;
 	});
 
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
 	eleventyConfig.addPlugin(EleventyI18nPlugin, {
 		defaultLanguage: "en",
 		filters: {
-			url: "locale_url",
+			htmlBaseUrl: "locale_url",
 			links: "locale_links"
 		},
 		errorMode: "strict"
